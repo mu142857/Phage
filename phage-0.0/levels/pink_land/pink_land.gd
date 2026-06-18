d94d096075bbbe42fb74f2e8d116201faf0082a0
Mumu <linjiamu49@gmail.com>
2026-06-18 11:51:22 -0700

    Update pink land

---
+++ phage-0.0/levels/pink_land/pink_land.gd
@@ -0,0 +1,19 @@
+extends Node2D
+
+@export var camera_limit_top: int = -90
+@export var camera_limit_bottom: int = 90
+@export var camera_limit_left: int = -80
+@export var camera_limit_right: int = 1600
+
+
+
+func _ready() -> void:
+	add_to_group("camera_follow")
+
+func get_camera_limits() -> Dictionary:
+	return {
+		"left": camera_limit_left,
+		"right": camera_limit_right,
+		"top": camera_limit_top,
+		"bottom": camera_limit_bottom,
+	}
